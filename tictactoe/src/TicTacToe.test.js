712f31cc62249dac0f3e8e094f359794be117d80
Neel Shah <shah_neel@hotmail.com>
2017-10-03 15:07:34 +0100

    Testing at component level

--- tictactoe/src/TicTacToe.test.js
+++ tictactoe/src/TicTacToe.test.js
@@ -1,41 +1,70 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
+import sinon from 'sinon';
 
-import TicTacToe from './TicTacToe';
-import Game from './TicTacToe';
+import Game, { Square, Board } from './TicTacToe';
 
-describe(TicTacToe, () => {
-  const game = new Game();
-  const component = shallow(
-    <TicTacToe />
-  )
+describe('TicTacToe game', () => {
 
-  it('renders and matches our snapshot', () => {
-    const component = renderer.create (
-      <TicTacToe />
+  it('tests square value gets rendered', () => {
+    const square = shallow(
+      <Square />
     )
-    const tree = component.toJSON
-    expect(tree).toMatchSnapshot();
+    expect(square.text()).toEqual('')
   });
 
-  it('displays a blank grid at the start of the game', () => {
-    expect(game).toHaveBeenCalled
+  it('calls the click action when square is clicked on', () => {
+    const onClickAction = sinon.spy();
+    const square = shallow(
+      <Square value="" onClick={onClickAction}/>
+    );
+    square.find('button').simulate('click');
+    expect(onClickAction.calledOnce).toEqual(true);
   });
 
-  it('should have 2 players', () => {
-    const xIsNext = ['X', 'O']
-    expect(xIsNext.length).toEqual(2)
+  it('displays 3 squares per row on the Board', () => {
+    const board = shallow(
+    <Board squares={ Array(9).fill(null)} />
+    );
+    expect(board.find('.board-row').length).toEqual(3);
   });
 
-  it('should have no winners at start of game', () => {
-    const status = 'Next player: '
-    expect(status).toEqual('Next player: ')
+  it('has 9 quares on the board', () => {
+    const board = mount(
+    <Board squares={ Array(9).fill(null)} />
+    );
+    expect(board.find('.square').length).toEqual(9);
+  });
+
+  it('first player is X and next player is O', () => {
+    const game = mount(
+    <Game />
+    );
+    game.find('button#0').simulate('click');
+    expect(game.find('button#0').text()).toEqual('X');
+    expect(game.find('.nextPlayer').text()).toEqual("Next player: O");
+  });
+
+  it('allows a player to win a game', () => {
+    const boardValues =
+    ["X", "O", "O",
+    null, "X", null,
+    null, null, null];
+
+    const game = mount(
+      <Game squares={boardValues}/>
+    );
+    game.find('button#8').simulate('click');
+     expect(game.find('.winner').text()).toEqual("Winner: X");
+  });
+
+  it('should only allow the winning lines', () => {
+    const boardValues = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    const component = mount(
+      <Game lines={boardValues} />
+    );
+    expect(component.find('lines').Array).toEqual(boardValues)
   })
-
-  it('should have player start with X', () => {
-    const xIsNext  = ['X', 'O']
-    expect(xIsNext).toContain('X')
-  });
 
 })
